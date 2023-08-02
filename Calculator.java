8fbf370ad9da24707acfdf2639ae39d18c35e913
lmerc17 <a1804049@adelaide.edu.au>
2023-08-02 15:26:50 +0930

    defined remote operations implemented by remote service within Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,5 +2,12 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Calculator extends Remote {
-    //insert functions to be implemented in CalculatorImplementation.java here
+
+    //defining the remote operations implemented by the remote service
+    void pushValue(int val) throws RemoteException;
+    void pushOperation(String operator) throws RemoteException;
+    int pop() throws RemoteException;
+    boolean isEmpty() throws RemoteException;
+    int delayPop(int millis) throws RemoteException;
+    
 }
