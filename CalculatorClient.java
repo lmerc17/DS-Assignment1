1c06925a6e963a0c36808c08121d47fe649e8fbd
lmerc17 <a1804049@adelaide.edu.au>
2023-08-08 14:58:04 +0930

    added some comments and some conditions to ensure no crashes occur when using pop and delayPop

--- CalculatorClient.java
+++ CalculatorClient.java
@@ -5,29 +5,40 @@ import java.io.InputStreamReader;
 
 public class CalculatorClient {
 
+	//default constructor
     private CalculatorClient() {}
 
     public static void main(String[] args) {
 
+	//definition of host depending on if arguments are given into main function
 	String host = (args.length < 1) ? null : args[0];
+
 		try {
+			//code to get the registry and create a stub for the client
 			Registry registry = LocateRegistry.getRegistry(host);
 			Calculator stub = (Calculator) registry.lookup("Calculator");
 			
+			//initialisation of bufferedReader for taking in input
 			BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-			String input;
-			String[] parsedInput;
-			int num;
-			boolean isEmpty;
+			String input; //input is stored in this string
+			String[] parsedInput; //input is parsed in this
+			int num; //numbers from the input is stored in this
+			boolean isEmpty; //boolean to store whether the stack is empty or not
 
 			//while loop to constantly take in input from the client
 			while(true){
+				//readLine from client and store it into input
 				input = bf.readLine();
-				parsedInput = input.split(" ");
+				parsedInput = input.split(" "); //split the input into the parsedInput array
 
+				//if two inputs have been given and the first one is push
 				if(parsedInput.length==2 && parsedInput[0].equals("push")){
-					num = Integer.parseInt(parsedInput[1]);
-					stub.pushValue(num);
+					try{
+						num = Integer.parseInt(parsedInput[1]); //convert the second string to an integer
+						stub.pushValue(num); //call pushValue with that number
+					} catch (NumberFormatException e){
+						System.out.println("Please enter a valid number");
+					}
 				}
 				else if(parsedInput.length==1 && (parsedInput[0].equals("min") || parsedInput[0].equals("max") || parsedInput[0].equals("lcm") || parsedInput[0].equals("gcd"))){
 					stub.pushOperation(parsedInput[0]);
@@ -50,8 +61,14 @@ public class CalculatorClient {
 					}
 				}
 				else if (parsedInput.length==2 && parsedInput[0].equals("delayPop")){
-					num = Integer.parseInt(parsedInput[1]);
-					System.out.println(stub.delayPop(num));
+					if(!stub.isEmpty()){
+						num = Integer.parseInt(parsedInput[1]);
+						System.out.println(stub.delayPop(num));
+					}
+					else{
+						System.out.println("Stack is empty, no value to pop");
+					}
+					
 				}
 				else if (parsedInput.length==1 && parsedInput[0].equals("exit")){
 					break;
